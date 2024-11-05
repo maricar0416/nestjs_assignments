c9c562806da336cfb5f6c03e527a14bb67bf5322
maricar0416 <gloriosomaricar0416@gmail.com>
2024-11-05 19:42:41 +0800

    Completed Assignment 1: Fibonacci Sequence

--- src/welcome/welcome.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('welcome')
-export class WelcomeController {
-  @Get()
-  getWelcomeMessage(): string {
-    return 'Welcome Kevin';
-  }
-}
